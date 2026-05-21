2613110c269008526c89645c09b05f27f53d983e
hasan7n <hasankassim7@hotmail.com>
2026-05-22 00:33:47 +0200

    add helper script for building ledger images using local repo

---
+++ pdo_images/build_pdo_images_local.sh
@@ -0,0 +1,32 @@
+set -e
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Some config
+PDO_CONTRACTS_DIR=/home/hasan/work/pdos/pdo-contracts
+TMP_PDO_CONTRACTS_DIR=/tmp/pdo-contracts
+PDO_VERSION=0.4.29
+PDO_DEBUG_BUILD=1
+PDO_LOG_LEVEL=debug
+
+# Copy the local repo into a tmp folder so the build can't disturb the
+# working tree. -a preserves perms/links and brings the submodule along.
+rm -rf $TMP_PDO_CONTRACTS_DIR
+cp -r $PDO_CONTRACTS_DIR $TMP_PDO_CONTRACTS_DIR
+
+PDO_SOURCE_ROOT=$TMP_PDO_CONTRACTS_DIR/private-data-objects
+cd $PDO_SOURCE_ROOT
+git checkout -B tmp_branch
+cp $SCRIPT_DIR/make.loc $PDO_SOURCE_ROOT/docker/
+
+# Build the pdo images
+PDO_SOURCE_ROOT=$PDO_SOURCE_ROOT \
+    PDO_VERSION=$PDO_VERSION \
+    PDO_DEBUG_BUILD=$PDO_DEBUG_BUILD \
+    PDO_LOG_LEVEL=$PDO_LOG_LEVEL \
+    make -C $PDO_SOURCE_ROOT/docker all
+PDO_SOURCE_ROOT=$PDO_SOURCE_ROOT \
+    PDO_VERSION=$PDO_VERSION \
+    PDO_DEBUG_BUILD=$PDO_DEBUG_BUILD \
+    PDO_LOG_LEVEL=$PDO_LOG_LEVEL \
+    make -C $PDO_SOURCE_ROOT/docker clean_repository
+
