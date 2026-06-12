5faa91c67a7d34dc781e4984abda0cbf1ef105c0
hasan7n <hasankassim7@hotmail.com>
2026-06-12 20:47:24 +0200

    rename policy engine

--- docker_start_policy_engine.sh
+++ docker_start_policy_engine.sh
@@ -1,6 +1,6 @@
 set -e
 INTERFACE="$(hostname -I | awk '{print $1}')"
-cd pdo_images && bash run_ledger.sh -i "mlcommons/pdo_ledger:latest" -n $INTERFACE -p "6600" &
+cd policy_engine && bash run_ledger.sh -i "mlcommons/pdo_ledger:latest" -n $INTERFACE -p "6600" &
 sleep 10
-cd pdo_images && bash run_services.sh -l http://$INTERFACE:6600 -i "mlcommons/pdo_services:latest" -n $INTERFACE &
+cd policy_engine && bash run_services.sh -l http://$INTERFACE:6600 -i "mlcommons/pdo_services:latest" -n $INTERFACE &
 sleep 15
